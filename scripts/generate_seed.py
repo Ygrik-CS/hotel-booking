44eeeceef6ddac82d1bef375c43a5f7b42886cd1
Ygrik <ygrik.im.123@gmail.com>
2025-09-27 17:18:20 +0500

    generate_seed.py script for data/seed.json

---
+++ scripts/generate_seed.py
@@ -0,0 +1,96 @@
+from datetime import date, timedelta
+import json
+import random
+import os
+import os
+os.makedirs("data", exist_ok=True)  # создаёт папку, если её ещё нет
+
+
+# Отели
+hotels = [
+    {"id": "h1", "name": "Hotel 1", "city": "Almaty", "stars": 3.9},
+    {"id": "h2", "name": "Hotel 2", "city": "Aktau", "stars": 5},
+    {"id": "h3", "name": "Hotel 3", "city": "Astana", "stars": 3.7},
+    {"id": "h4", "name": "Hotel 4", "city": "Astana", "stars": 4.9},
+    {"id": "h5", "name": "Hotel 5", "city": "Almaty", "stars": 4.1}
+]
+
+room_types = []
+rates = []
+prices = []
+availability = []
+
+start_date = date(2025, 10, 1)
+days_count = 60
+
+room_type_counter = 1
+rate_counter = 1
+
+# Комнаты и тарифы
+for hotel in hotels:
+    for j in range(4):  
+        room_type_id = f"room_type{room_type_counter}"
+        room_types.append({
+            "id": room_type_id,
+            "hotel_id": hotel["id"],
+            "name": f"Type-{j+1}"
+        })
+
+        # 3 тарифа
+        for tariff in ["Standard", "Non-refundable", "Breakfast included"]:
+            rate_id = f"r{rate_counter}"
+            rates.append({
+                "id": rate_id,
+                "room_type_id": room_type_id,
+                "name": tariff
+            })
+
+            # цены на 60 дней
+            for day in range(days_count):
+                current_date = start_date + timedelta(days=day)
+                price = random.randint(10000, 30000)
+                prices.append({
+                    "rate_id": rate_id,
+                    "date": current_date.isoformat(),
+                    "price": price
+                })
+
+            rate_counter += 1
+        room_type_counter += 1
+
+# Доступность
+for room_type in room_types:
+    for day in range(days_count):
+        current_date = start_date + timedelta(days=day)
+        availability.append({
+            "room_type_id": room_type["id"],
+            "date": current_date.isoformat(),
+            "available": random.randint(1, 10)
+        })
+
+
+# Гости
+guests = []  
+for i in range(1, 50 + 1): 
+    guest = {         
+        "id": "guest_" + str(i),
+        "name": "Guest_№" + str(i)
+    }
+    guests.append(guest)  
+
+
+# Сборка всего в один seed
+seed = {
+    "hotels": hotels,
+    "room_types": room_types,
+    "rates": rates,
+    "prices": prices,
+    "availability": availability,
+    "guests": guests
+}
+
+# Запись в JSON файл
+with open("./data/seed.json", "w", encoding="utf-8") as f:
+    json.dump(seed, f, indent=4, ensure_ascii=False)
+
+print("seed.json создан!")
